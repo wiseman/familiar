b7e44609edbecd0caf7e07651eb31315130b2ad8
wiseman <jjwiseman@gmail.com>
2012-05-11 13:39:02 -0700

    Base app class.

---
+++ hml/base/app.py
@@ -0,0 +1,41 @@
+import argparse
+import logging
+import sys
+
+
+LOGGING_LEVELS = {
+  'DEBUG': logging.DEBUG,
+  'INFO': logging.INFO,
+  'WARNING': logging.WARNING,
+  'ERROR': logging.ERROR,
+  'CRITICAL': logging.CRITICAL
+  }
+
+
+def get_logging_level_by_name(name):
+  return LOGGING_LEVELS[name]
+
+
+class App(object):
+  def __init__(self, main=None):
+    self.main = main
+    assert main
+
+  def run(self, argv=None):
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+      '--logging_level',
+      dest='logging_level',
+      choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'],
+      default='INFO',
+      help='The logging level to use.')
+
+    if not argv:
+      argv = sys.argv
+
+    args, unknown_args = arg_parser.parse_known_args(args=argv)
+    logging.basicConfig(
+      level=get_logging_level_by_name(args.logging_level),
+      format='%(asctime)s:%(levelname)s:%(module)s:%(lineno)d: %(message)s')
+
+    self.main(unknown_args)
